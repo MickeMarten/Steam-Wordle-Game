91447fd8b043d91889497c0528dc1d8413ed5d07
MickeMarten <Micke.Marten123@gmail.com>
2024-04-02 17:44:15 +0200

    Något

--- Server/routes/aboutUs.js
+++ Server/routes/aboutUs.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const aboutUsRouter = Router();
 
 aboutUsRouter.get('/aboutus', async (req, res) => {
-  res.render('highscore');
+  res.render('aboutus');
 });
 
 export default aboutUsRouter;
