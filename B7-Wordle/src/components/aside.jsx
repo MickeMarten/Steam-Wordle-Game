22fec05a8e2836db86360024ed117cf5175a34be
MickeMarten <Micke.Marten123@gmail.com>
2024-04-03 14:13:05 +0200

    Added styling instead of database.

--- B7-Wordle/src/components/aside.jsx
+++ B7-Wordle/src/components/aside.jsx
@@ -1,7 +1,29 @@
 function SidePanel() {
   return (
-    <aside className="steamGreen w-28 h-full fixed top-0 left-0 bottom-0">
-      <a href="http://localhost:5080/AboutUs">Länk</a>
+    <aside className="steamGreen mw-48 h-full fixed top-0 left-0 bottom-0 p-4 border-r-2 flex flex-col gap-10">
+      <h1 className="text-white mb-4 text-3xl">Wordle Game</h1>
+      <ul className="text-white flex flex-col gap-28 text-2xl">
+        <li>
+          <a href="/aboutus" className="block hover:bg-green-800 ">
+            About Wordle-gaming
+          </a>
+        </li>
+        <li>
+          <a href="/" className="block hover:bg-green-800">
+            Play the Wordle game
+          </a>
+        </li>
+        <li>
+          <a href="/highscore" className="block hover:bg-green-800">
+            Check out the wordle highscore
+          </a>
+        </li>
+        <li>
+          <a href="/aboutus" className="block hover:bg-green-800">
+            Check out more games
+          </a>
+        </li>
+      </ul>
     </aside>
   );
 }
--- B7-Wordle/src/components/gameComponents/gameFrame.jsx
+++ B7-Wordle/src/components/gameComponents/gameFrame.jsx
@@ -148,6 +148,7 @@ function GameFrame() {
         showModal={showModal}
         onClick={() => {
           postToServer('/api/playerScoreData', resultsArray);
+          window.location.href = '/highscore';
         }}
       />
     </section>
