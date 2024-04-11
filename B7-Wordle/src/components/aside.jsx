3e673032deb665fe5a95d7471363a5630d0a82a9
MickeMarten <KarlMikael.Martensson@gmail.com>
2024-04-11 22:04:59 +0200

    Added styling to links

--- B7-Wordle/src/components/aside.jsx
+++ B7-Wordle/src/components/aside.jsx
@@ -1,26 +1,26 @@
 function SidePanel() {
   return (
-    <aside class="steamGreen max-w-48 h-full border-r-2 p-4">
+    <aside class="steamGreen max-w-48 h-full border-r p-4">
       <div className="h-screen flex flex-col gap-10">
-        <h1 className="text-white mb-4 text-3xl">Wordle Gamings</h1>
-        <ul className="text-white flex flex-col gap-28 text-2xl">
-          <li>
-            <a href="/aboutus" className="block hover:bg-green-800">
+        <h1 className="text-white mb-4 sm:text-3xl">Wordle Gamings</h1>
+        <ul className="text-white flex flex-col gap-28 sm:text-2xl">
+          <li className="steamDark hover:bg-green-800 text-white font-bold py-2 px-4 rounded border">
+            <a href="/aboutus" className="">
               About Wordle-gaming
             </a>
           </li>
-          <li>
-            <a href="/" className="block hover:bg-green-800">
+          <li className="steamDark hover:bg-green-800 text-white font-bold py-2 px-4 rounded border">
+            <a href="/" className="">
               Play the Wordle game
             </a>
           </li>
-          <li>
-            <a href="/highscore" className="block hover:bg-green-800">
+          <li className="steamDark hover:bg-green-800 text-white font-bold py-2 px-4 rounded border">
+            <a href="/highscore" className="">
               Check out the wordle highscore
             </a>
           </li>
-          <li>
-            <a href="/aboutus" className="block hover:bg-green-800">
+          <li className="steamDark hover:bg-green-800 text-white font-bold py-2 px-4 rounded border">
+            <a href="/aboutus" className="">
               Check out more games
             </a>
           </li>
